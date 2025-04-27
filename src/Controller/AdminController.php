4bbae7c231a76b25125edfc1e346fb6a5be0d199
Oumayma Saddouri <saddourioumayma@gmail.com>
2025-04-27 22:00:32 +0100

    Second commit last validation symfony

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -12,26 +12,26 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class AdminController extends AbstractController
 {
     #[Route('/admin', name: 'admin')]
-    public function index(ManagerRegistry $doctrine): Response //function index admin
+    public function index(ManagerRegistry $doctine): Response
     {
-        $user = $this->getUser();// Get the current user
+        $user = $this->getUser();
         if (!$user) {
-            throw $this->createAccessDeniedException('You are not authorized to access this resource.'); // Check if user is logged in
+            throw $this->createAccessDeniedException('You are not authorized to access this resource.');
         }
 
         return $this->render('Admin/index.html.twig', [
-        ]); // Render the admin index page
+        ]);
     } 
 
-    #[Route('/trip/status-count', name: 'trip_status_count', options:['expose' => true])] //URL for the status count function
-    public function statusCount(ManagerRegistry $doctrine): JsonResponse //function to get the status count and return it as JSON
+    #[Route('/trip/status-count', name: 'trip_status_count', options:['expose' => true])]
+    public function statusCount(ManagerRegistry $doctrine): JsonResponse
     {
-        $em = $doctrine->getManager(); // Get the entity manager for selecting the trip data from the database
+        $em = $doctrine->getManager();
 
         // Get the count of trips by status
-        $tripRepository = $em->getRepository(Trip::class); //get all the trip data from the database
+        $tripRepository = $em->getRepository(Trip::class);
 
-        $statusCounts = $tripRepository->createQueryBuilder('t') // Create a query builder for the Trip entity
+        $statusCounts = $tripRepository->createQueryBuilder('t')
             ->select('t.status, COUNT(t.id) AS count')
             ->groupBy('t.status')
             ->getQuery()
